60479906a4af75290a712442feca17bfef9d966c
Sertan Senturk <contact@sertansenturk.com>
2020-04-03 10:00:58 +0100

    Added data fixtures to mlflow tests

--- tests/docker/test_mlflow.py
+++ tests/docker/test_mlflow.py
@@ -2,9 +2,8 @@ import mlflow
 import pytest
 
 
-@pytest.mark.dependency()
-def test_mlflow_log_to_backend():
-    # GIVEN
+@pytest.fixture(scope="module")
+def logs():
     parameters = {
         "param1": "param1_val",
         "param2": "param2_val"
@@ -18,15 +17,37 @@ def test_mlflow_log_to_backend():
         "val": "tag_val1"
     }
 
+    return {
+        "parameters": parameters,
+        "metric": metric,
+        "tag": tag
+    }
+
+
+@pytest.fixture(scope="module")
+def artifact():
+    dummy_artifact = "dummy.txt"
+    with open(dummy_artifact, 'w'):
+        pass  # create an empty file
+
+    return dummy_artifact
+
+
+@pytest.mark.dependency()
+def test_mlflow_log_to_backend(logs):
+    # GIVEN
+    experiment_name = 'test_experiment'
+    run_name = "test_run"
+
     # WHEN
-    mlflow.set_experiment('experiment-alpha')
-    with mlflow.start_run():
-        mlflow.log_params(parameters)
+    mlflow.set_experiment(experiment_name)
+    with mlflow.start_run(run_name=run_name):
+        mlflow.log_params(logs["parameters"])
 
-        for vv in metric["val"]:
-            mlflow.log_metric(metric["key"], vv)
+        for vv in logs["metric"]["val"]:
+            mlflow.log_metric(logs["metric"]["key"], vv)
 
-        mlflow.set_tag(tag["key"], tag["val"])
+        mlflow.set_tag(logs["tag"]["key"], logs["tag"]["val"])
 
     # THEN
     assert True  # logging succeeded
@@ -38,15 +59,15 @@ def test_mlflow_get_logs():
 
 
 @pytest.mark.dependency()
-def test_mlflow_log_artifact():
+def test_mlflow_log_artifact(artifact):
     # GIVEN
-    with open("dummy.txt", 'w'):
-        pass  # create an empty file
+    experiment_name = 'test_experiment'
+    run_name = "test_run"
 
     # WHEN
-    mlflow.set_experiment('experiment-alpha')
-    with mlflow.start_run():
-        mlflow.log_artifact("dummy.txt")
+    mlflow.set_experiment(experiment_name)
+    with mlflow.start_run(run_name=run_name):
+        mlflow.log_artifact(artifact)
 
     # THEN
     assert True  # logging succeeded
