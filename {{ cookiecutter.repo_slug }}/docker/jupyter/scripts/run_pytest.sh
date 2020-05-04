4eb6cdd53fb8cff16a9fcde9099c87052d0a1a0b
Sertan Senturk <contact@sertansenturk.com>
2020-05-04 23:33:58 +0100

    CC-28 Added WAIT_FOR_IT_TIMEOUT

--- {{ cookiecutter.repo_slug }}/docker/jupyter/scripts/run_pytest.sh
+++ {{ cookiecutter.repo_slug }}/docker/jupyter/scripts/run_pytest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-./wait-for-it.sh mlflow:5000 -t 60 -- \
+./wait-for-it.sh $1:$2 -t $3 -- \
     echo "Running pytest as $USER with uid $UID" && \
     pytest -vv "$(pwd)/docker"
