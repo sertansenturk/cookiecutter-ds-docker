8aaa466b522d737e04c0ca2c2bc3725d8b9d8d49
Sertan Senturk <contact@sertansenturk.com>
2020-05-04 22:47:01 +0100

    CC-28 Waiting mlflow to get ready before running tests in the jupyter container

--- {{ cookiecutter.repo_slug }}/docker/jupyter/scripts/run_pytest.sh
+++ {{ cookiecutter.repo_slug }}/docker/jupyter/scripts/run_pytest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-sleep 5
-echo "Running pytest as $USER with uid $UID"
-pytest -vv "$(pwd)/docker"
+./wait-for-it.sh mlflow:5000 -t 60 -- \
+    echo "Running pytest as $USER with uid $UID" && \
+    pytest -vv "$(pwd)/docker"
