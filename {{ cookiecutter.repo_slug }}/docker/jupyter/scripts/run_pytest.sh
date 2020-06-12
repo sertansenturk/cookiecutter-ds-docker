e368f0b3da9c6a85bc9a60808eb5d39e515b2818
Sertan Senturk <contact@sertansenturk.com>
2020-06-12 02:17:32 +0100

    CC-51 Adding (broken) test to check editable package install

--- {{ cookiecutter.repo_slug }}/docker/jupyter/scripts/run_pytest.sh
+++ {{ cookiecutter.repo_slug }}/docker/jupyter/scripts/run_pytest.sh
@@ -4,3 +4,7 @@ set -e
 ./wait-for-it.sh $1:$2 -t $3 -- \
     echo "Running pytest as $USER with uid $UID" && \
     pytest -vv "$(pwd)/docker"
+
+# test if the package is installed in editable mode
+# i.e. if the {{ cookiecutter.repo_slug}}.egg-link is in site-packages
+ls /opt/conda/lib/python3.7/site-packages/mre.egg-linkd
