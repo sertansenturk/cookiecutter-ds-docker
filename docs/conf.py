b85b968fd2621efc2d117a2789a8b5306f359f2b
Sertan Senturk <contact@sertansenturk.com>
2020-06-21 14:41:06 +0100

    CC-54 Minor indentation fix in docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -58,8 +58,7 @@ html_static_path = ['_static']
 html_context = {
     'css_files': [
         '_static/theme_overrides.css',  # override wide tables in RTD theme
-        ],
-     }
+        ]}
 
 # Sphinx master doc
 master_doc = 'index'
